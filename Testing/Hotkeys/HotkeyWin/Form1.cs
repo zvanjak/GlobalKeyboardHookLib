ff13e5a8313dadb2604e9d9be68891a19e1f05fa
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2017-01-13 00:26:20 +0100

    Adding testing projects - implementations found on internet

---
+++ Testing/Hotkeys/HotkeyWin/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Hotkeys;
+
+namespace HotkeyWin
+{
+    public partial class Form1 : Form
+    {
+        private Hotkeys.GlobalHotkey ghk;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ghk = new Hotkeys.GlobalHotkey(Constants.ALT + Constants.CTRL, Keys.D, this);
+        }
+
+        private void HandleHotkey()
+        {
+            WriteLine("Hotkey pressed!");
+            Form2 dlg = new Form2();
+            dlg.ShowDialog();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          if (!ghk.Register())
+            MessageBox.Show("Couldn't register");
+          
+          /* WriteLine("Trying to register SHIFT+ALT+O");
+            if (ghk.Register())
+                WriteLine("Hotkey registered.");
+            else
+                WriteLine("Hotkey failed to register");
+           */
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ghk.Unregiser())
+                MessageBox.Show("Hotkey failed to unregister!");
+        }
+
+        private void WriteLine(string text)
+        {
+            textBox1.Text += text + Environment.NewLine;
+        }
+    }
+}
---
+++ Testing/TestGlobalKeyboardHooks/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+class InterceptKeys
+{
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private const int WM_SYSKEYDOWN = 0x0104; 
+    private static LowLevelKeyboardProc _proc = HookCallback;
+    private static IntPtr _hookID = IntPtr.Zero;
+
+    public static void Main()
+    {
+        _hookID = SetHook(_proc);
+        Application.Run();
+        UnhookWindowsHookEx(_hookID);
+    }
+
+    private static IntPtr SetHook(LowLevelKeyboardProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+    private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN) )
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+            Console.WriteLine((Keys)vkCode);
+//            Console.WriteLine(Control.ModifierKeys);
+            
+            if (Keys.D == (Keys)vkCode && ( Control.ModifierKeys & (Keys.ControlKey | Keys.Alt)) != 0)
+            {
+                Console.WriteLine("Ctrl + Alt + D");
+              MessageBox.Show("Prdo");
+            }
+            if( (Keys)vkCode == Keys.E )
+            {
+                Console.WriteLine("Alt + Shift + E");
+            }
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+        IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+}
